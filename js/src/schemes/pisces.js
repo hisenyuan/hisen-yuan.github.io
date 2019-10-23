8e3afdc150aa0b61dc56ac5a367d97dba4ab2b05
hisenyuan <hisenyuan@gmail.com>
2019-10-23 12:16:41 +0800

    Site updated: 2019-10-23 12:16:40

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Wed Oct 23 2019 12:04:05 GMT+0800 (China Standard Time)
+// build time:Wed Oct 23 2019 12:16:38 GMT+0800 (China Standard Time)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var i=function(){return n.height()+10};var a=function(n){return r.css({"margin-top":n})};var e=window.matchMedia("(min-width: 991px)");a(i()).show();e.addListener(function(n){if(n.matches){a(i())}})});
 //rebuild by neat 
