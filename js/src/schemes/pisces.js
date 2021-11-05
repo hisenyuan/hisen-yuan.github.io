a36c8f1bba43a293911c2c7fe1285e8b97243b35
hisenyuan <yuanhaixing@baidu.com>
2021-11-05 23:20:40 +0800

    Site updated: 2021-11-05 23:20:38

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Sat Oct 09 2021 12:10:41 GMT+0800 (China Standard Time)
+// build time:Fri Nov 05 2021 23:20:36 GMT+0800 (China Standard Time)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var i=function(){return n.height()+10};var a=function(n){return r.css({"margin-top":n})};var e=window.matchMedia("(min-width: 991px)");a(i()).show();e.addListener(function(n){if(n.matches){a(i())}})});
 //rebuild by neat 
